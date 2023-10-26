27a81de22d5ef5aeb94647d05063f20a9a0f59ff
Thắng Đinh <thangdthe163539@fpt.edu.vn>
2023-10-26 19:23:08 +0700

    Revert "resolve conflict"
    This reverts commit 1557617445d90db3ba13e0799ee33890c618f81c.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,21 +3,23 @@ import Header2 from '@/components/layouts/Header/index2';
 import '@/styles/globals.css';
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import Footer from '@/components/layouts/Footer';
-import Feature from '@/components/layouts/Feature';
 
 export default function App({ Component, pageProps }) {
   //
   const router = useRouter();
-  const pathsToShowHeader = ['home', 'sign-up', 'login', ''];
+  const pathsToShowHeader = ['home', ''];
   const shouldShowHeader = pathsToShowHeader.includes(router.pathname);
   //
   return (
     <ChakraProvider>
-      <Box>
-        {router.pathname === '/' || shouldShowHeader ? <Header /> : <Header2 />}
-      </Box>
-      <Box style={{ backgroundColor: 'white', minHeight: '100hv' }}>
+      <Box minHeight={'100vh'}>
+        {router.pathname === '/' ? (
+          ''
+        ) : shouldShowHeader ? (
+          <Header />
+        ) : (
+          <Header2 />
+        )}
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
