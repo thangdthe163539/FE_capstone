b2a3f6a812078f295fbbbcdc2e8d513cd1267fb5
Loc <socmummim220401@gmail.com>
2023-11-30 10:58:00 +0700

    dashboard

--- src/pages/adminpages/adminhome.js
+++ src/pages/adminpages/adminhome.js
@@ -8,21 +8,28 @@ function ADHomePage() {
     <Box className={styles.bodybox}>
       <List>
         <ListItem className={styles.list}>
+          <Link className={styles.listitem} href='/adminpages/dashboard'>
+            Dashboard
+          </Link>
+        </ListItem>
+        <ListItem className={styles.list}>
           <Link className={styles.listitem} href='/adminpages/userManager'>
             User Management
           </Link>
         </ListItem>
         <ListItem className={styles.list}>
-            <Link className={styles.listitem} href='/adminpages/feedbackManager'>
-              Feedback Management
-            </Link>
+          <Link className={styles.listitem} href='/adminpages/feedbackManager'>
+            Feedback Management
+          </Link>
         </ListItem>
         <ListItem className={styles.list}>
           <Flex style={{ fontSize: '18px' }}>
             <Link className={styles.listitem} href='/adminpages/issuehome'>
               Issue Management
             </Link>
-            <Text style={{ marginLeft: '5px' }}>(These are errors posted by admins asking product owners to fix)</Text>
+            <Text style={{ marginLeft: '5px' }}>
+              (These are errors posted by admins asking product owners to fix)
+            </Text>
           </Flex>
         </ListItem>
       </List>
